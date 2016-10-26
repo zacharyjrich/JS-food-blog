1bda16ee784de3add8528295940eb2eb6dda0da8
Kyle Lange and Zachary Richards <kyle@>
2016-10-26 15:52:28 -0700

    add delete functionality to ONLY the admin route.  Delete button does not appear on index.hbs.

---
+++ app/components/admin-tile.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    delete(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        console.log(post);
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
+});
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model() {
     return this.store.findAll('post');
   },
+
+  actions: {
+    destroyPost(post) {
+      debugger;
+      post.destroyRecord();
+      this.transitionTo('admin');
+    },
+    debug(){
+      debugger;
+    }
+  }
 });
